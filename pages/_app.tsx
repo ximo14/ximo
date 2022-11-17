9b0e9b17c6171225ca9369508a64e1730a8e46b0
ximo <ximo.gil@widitrade.com>
2022-11-17 15:39:36 +0100

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider
+			enableSystem={true}
+			attribute='class'>
+			<Component {...pageProps} />
+			<Analytics />
+		</ThemeProvider>
+	)
 }
 
 export default MyApp
