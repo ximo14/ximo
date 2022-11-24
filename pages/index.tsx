075a1e81da64d6bd4c41a3710228e2de1b1b0d48
ximo <ximo.gil@widitrade.com>
2022-11-24 15:52:05 +0100

    feat: add tinybird

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,31 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Nav from '../components/Nav/Nav';
-import Footer from '../components/Footer/Footer';
-import Hero from '../components/Hero/Hero';
-import Experience from '../components/Experience/Experience';
-import Technologies from '../components/Technologies/Technologies';
-import PersonalProjects from '../components/PersonalProjects/PersonalProjects';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Nav from '../components/Nav/Nav'
+import Footer from '../components/Footer/Footer'
+import Hero from '../components/Hero/Hero'
+import Experience from '../components/Experience/Experience'
+import Technologies from '../components/Technologies/Technologies'
+import PersonalProjects from '../components/PersonalProjects/PersonalProjects'
 
 const Home: NextPage = () => {
+	const TINYBIRD_TOKEN = process.env.TINYBIRD_TOKEN
 	return (
 		<div>
 			<Head>
 				<title>Ximo Gil</title>
-				<meta name='description' content='Software Developer' />
-				<link rel='icon' href='/favicon.ico' />
+				<meta
+					name='description'
+					content='Software Developer'
+				/>
+				<link
+					rel='icon'
+					href='/favicon.ico'
+				/>
+				<script
+					defer
+					src='https://unpkg.com/@tinybirdco/flock.js'
+					data-host='https://api.tinybird.co'
+					data-token={TINYBIRD_TOKEN}></script>
 			</Head>
 
 			<main className='h-full w-full px-5 md:px-10 py-5 bg-white text-black dark:bg-zinc-900 dark:text-white transition-colors ease-in-out delay-200'>
@@ -32,4 +44,4 @@ const Home: NextPage = () => {
 	)
 }
 
-export default Home;
+export default Home
